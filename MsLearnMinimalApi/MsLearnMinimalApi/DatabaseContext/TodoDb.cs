1ae37e531182f1b08140fb67554c2ad7e0c1cf10
LeeJongPil <dlwhdvlf71@gmail.com>
2025-02-01 17:31:11 +0900

    General 사용방법 & MapGroup

---
+++ MsLearnMinimalApi/MsLearnMinimalApi/DatabaseContext/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MsLearnMinimalApi.DatabaseContext
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+        {
+        }
+
+        public DbSet<Model.Todo> Todo => Set<Model.Todo>();
+    }
+}
--- MsLearnMinimalApi/MsLearnMinimalApi/Program.cs
+++ MsLearnMinimalApi/MsLearnMinimalApi/Program.cs
@@ -1,6 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using MsLearnMinimalApi.DatabaseContext;
+using MsLearnMinimalApi.Model;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<TodoDb>(option => option.UseInMemoryDatabase("TodoDb"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter(); //  데이터베이스 관련 예외를 표시하도록 한다.
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+#region General 사용방법
+
+//app.MapGet("/todo", async (TodoDb db) =>
+//{
+//    var todo = await db.Todo.ToListAsync();
+//    return Results.Ok(todo);
+//});
+
+//app.MapPost("/todo", async (Todo todo, TodoDb db) =>
+//{
+//    db.Todo.Add(todo);
+//    await db.SaveChangesAsync();
+//    return Results.Created($"/todo/{todo.Id}", todo);
+//});
+
+#endregion
+
+#region MapGroup 사용방법
+
+var todoMapGroup = app.MapGroup("/todo");
+
+todoMapGroup.MapGet("/", async (TodoDb db) =>
+{
+    var todo = await db.Todo.ToListAsync();
+    return Results.Ok(todo);
+});
+
+todoMapGroup.MapPost("/", async (Todo todo, TodoDb db) =>
+{
+    db.Todo.Add(todo);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todo/{todo.Id}", todo);
+});
+
+todoMapGroup.MapGet("/{id}", async (int id, TodoDb db) =>
+{
+    var todo = await db.Todo.FindAsync(id);
+    if (todo is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(todo);
+});
+
+#endregion
+
 
 app.Run();
